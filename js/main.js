0393267a64f02c940b5350354ee091c55be8024c
andryushque <andrew.ustinov23@gmail.com>
2020-10-05 12:09:17 +0300

    Create: media for header section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-  // js code
+  /*=== Mobile Menu ===*/
+  // Show and hide navigation menu
+  const mmenuButtonOpen = $(".mmenubtn--open");
+  const mmenuButtonClose = $(".mmenubtn--close");
+  const mobileMenu = $(".mmenu");
+
+  mmenuButtonOpen.on("click", function () {
+    mobileMenu.addClass("mmenu--visible");
+  });
+  mmenuButtonClose.on("click", function () {
+    mobileMenu.removeClass("mmenu--visible");
+  });
+
+  // Close mobile menu on wide screens
+  $(window)
+    .resize(function () {
+      if (screen.width > 991 && $(window).width() > 991) {
+        mobileMenu.removeClass("mmenu--visible");
+      }
+    })
+    .resize();
 });
